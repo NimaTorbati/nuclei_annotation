40f0ff39feea873efc9f137d2fc69ce8de3c6945
Nima <nima.torbati@gmail.com>
2024-09-17 18:40:04 +0200

    Added a color_code vector to keep the order of colors.

--- manual_masks_generator_multi_classes/masks_generator.m
+++ manual_masks_generator_multi_classes/masks_generator.m
@@ -79,8 +79,25 @@ for counter = 1:length(imagej_zips) %loop over images
     end
    % Get unique values
    uniqueStrokeColors = unique(strokeColors);
-   color_opt = {'#77AC30', 'r', '#FFFFFF'}; %green = "#77AC30", '#FFFFFF' = white
 
+   %Check if any new class has been added and keep the colors order same as
+   %before
+   if counter == 1
+       uniqueStrokeColors1 = uniqueStrokeColors;%ignore it for the first iteration
+   else
+       if mean(uniqueStrokeColors1 ~= uniqueStrokeColors) ~= 1
+           for i = 1:length(uniqueStrokeColors)
+               if isempty(find(uniqueStrokeColors(i) == uniqueStrokeColors1))
+                   uniqueStrokeColors1 = [uniqueStrokeColors1, uniqueStrokeColors(i)];
+               end
+           end
+          uniqueStrokeColors = uniqueStrokeColors1;
+       end
+   end
+
+
+   color_opt = {'#77AC30', 'r', '#FFFFFF'}; %green = "#77AC30", '#FFFFFF' = white
+   color_code = [4278255360 4294901760 4294967295];
    for ii = 1:length(uniqueStrokeColors)
        nuc_class_count = 0;
        mask_overlap = zeros(size_target,size_target);
@@ -88,9 +105,11 @@ for counter = 1:length(imagej_zips) %loop over images
        mask_overlap_borderremove = zeros(size_target,size_target);
        D_overlap = zeros(size_target, size_target);
        for n_num=1:length(ROIs) % loop over rois in one image
-           ROIName = strcat('.\tempfolder\',ROIs(n_num).name); 
+           ROIName = strcat('.\tempfolder\',ROIs(n_num).name);
            [sROI] = ReadImageJROI(ROIName);
            if sROI.nStrokeColor == uniqueStrokeColors(ii)
+               color = sROI.nStrokeColor;
+               c_ind = find(color == color_code);
                nuc_class_count = nuc_class_count+1;
                mask = poly2mask(sROI.mnCoordinates(:,1),sROI.mnCoordinates(:,2), 512, 512);
                %stacked_mask(:,:,n_num)= mask;
@@ -103,7 +122,7 @@ for counter = 1:length(imagej_zips) %loop over images
                %% for border remove
                eshterak = find(mask_overlap_borderremove == mask_org & mask_overlap_borderremove==1);
                eshterak_img = zeros(512,512);
-               eshterak_img(eshterak)=1; 
+               eshterak_img(eshterak)=1;
                mask_overlap_borderremove = max(mask_org, mask_overlap_borderremove);
                if length(eshterak)~=0
                    B = edge(mask_org,'nothinning');
@@ -111,76 +130,77 @@ for counter = 1:length(imagej_zips) %loop over images
                    se = strel('disk', 1);
                    B = B & eshterak_img;
                    B2 = imdilate(B,strel(se));
-           
-                   mask_overlap_borderremove = mask_overlap_borderremove - B2; 
-                   mask_overlap_borderremove = mask_overlap_borderremove - thin_edge; 
+
+                   mask_overlap_borderremove = mask_overlap_borderremove - B2;
+                   mask_overlap_borderremove = mask_overlap_borderremove - thin_edge;
                    mask_overlap_borderremove(mask_overlap_borderremove==-1)=0;
                end
            end
        end
-       object_count_tot (ii + 1, counter) = nuc_class_count;
-               %% for masks with diffent label for each object    
-               savepath_labelmask = strcat(results_path,'class_' , string(ii),'_annotations\','label_masks','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
+       object_count_tot (c_ind + 1, counter) = nuc_class_count;
+               %% for masks with diffent label for each object
+               savepath_labelmask = strcat(results_path,'class_' , string(c_ind),'_annotations\','label_masks','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
                imwrite(uint16(mask_overlap),savepath_labelmask);
-               %% for masks with diffent label for each object (overlaying areas assign to one object!)    
-               savepath_labelmask = strcat(results_path,'class_' , string(ii),'_annotations\','label_masks_modify','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
+               %% for masks with diffent label for each object (overlaying areas assign to one object!)
+               savepath_labelmask = strcat(results_path,'class_' , string(c_ind),'_annotations\','label_masks_modify','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
                imwrite(uint16(mask_overlap_modify),savepath_labelmask);
                %% for binary mask
                mask_binary = zeros(512,512);
                mask_binary (mask_overlap>0)= 255;
                mask_binary = uint8(mask_binary);
-               savepath_binary = strcat(results_path,'class_' , string(ii),'_annotations\','mask_binary','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
+               savepath_binary = strcat(results_path,'class_' , string(c_ind),'_annotations\','mask_binary','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
                imwrite(mask_binary,savepath_binary);
 
                %% for nuclie border
                for diskthick =1:10
                    se = strel('disk', diskthick);
                    eroded_mask_binary = imerode(mask_binary,strel(se));
-                   border = mask_binary - eroded_mask_binary; 
-                   savepath_border = strcat(results_path,'class_' , string(ii),'_annotations\','nuclei_border\disk',string(diskthick),'\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
+                   border = mask_binary - eroded_mask_binary;
+                   savepath_border = strcat(results_path,'class_' , string(c_ind),'_annotations\','nuclei_border\disk',string(diskthick),'\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
                    imwrite(border, savepath_border);
                end
 
                %% for mask removing borders like TMI paper
-               savepath_binary_borderremoved = strcat(results_path,'class_' , string(ii),'_annotations\','mask_binary_without_border','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
+               savepath_binary_borderremoved = strcat(results_path,'class_' , string(c_ind),'_annotations\','mask_binary_without_border','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
                mask_overlap_borderremove (mask_overlap_borderremove>0)= 255;
                imwrite(uint8(mask_overlap_borderremove),savepath_binary_borderremoved);
-    
+
                %% for weighted maps
-               gt = mask_overlap_borderremove; 
+               gt = mask_overlap_borderremove;
                se = strel('disk', 1);
                gt_erode = imerode(gt,strel(se));
-    
+
                [weight]=unetwmap(gt);
                [weight_erode]=unetwmap(gt_erode);
                weight = weight* 255/max(weight(:));
                weight_erode = weight_erode* 255/max(weight_erode(:));
-    
-               weighted_maps_path = strcat(results_path,'class_' , string(ii),'_annotations\','weighted_maps','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
-               weighted_maps_erode_path = strcat(results_path,'class_' , string(ii),'_annotations\','weighted_maps_erode','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
+
+               weighted_maps_path = strcat(results_path,'class_' , string(c_ind),'_annotations\','weighted_maps','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
+               weighted_maps_erode_path = strcat(results_path,'class_' , string(c_ind),'_annotations\','weighted_maps_erode','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
                imwrite(uint8(weight),weighted_maps_path,'Mode','lossless');
                imwrite(uint8(weight_erode),weighted_maps_erode_path,'Mode','lossless');
-    
+
                %% to save the math files (not needed)
                % savepath_math = strcat(results_path,'math_weighted_maps','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
                % savepath_math_erode = strcat(results_path,'math_weighted_maps_erode','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
                % save(savepath_math,'weight')
                % save(savepath_math_erode,'weight_erode')
-    
+
                %% for mask binary without border erode
                se = strel('disk', 1);
                mask_binary_without_border_erode = imerode(mask_overlap_borderremove,strel(se));
-               savepath_binary_borderremoved = strcat(results_path,'class_' , string(ii),'_annotations\','mask_binary_without_border_erode','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
+               savepath_binary_borderremoved = strcat(results_path,'class_' , string(c_ind),'_annotations\','mask_binary_without_border_erode','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
                imwrite(uint8(mask_binary_without_border_erode),savepath_binary_borderremoved);
-    
+
                %% for distance maps
-               savepath_distance = strcat(results_path,'class_' , string(ii),'_annotations\','distance_maps','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
+               savepath_distance = strcat(results_path,'class_' , string(c_ind),'_annotations\','distance_maps','\', strcat(erase(raw_imgs(counter).name,'.png'),'.png'));
                D_overlap = double(D_overlap);
                D_overlap = D_overlap/max(D_overlap(:)); % otherwise you get bianry image for distance map
                imwrite(D_overlap,savepath_distance,'Mode','lossless');
-    
+
                %% for overliad images
                original= imread(strcat(raw_imgs(counter).folder,'\',raw_imgs(counter).name));
+               original = imresize(original, [size_target size_target]);
                img_dim = size(original);
                if length(img_dim) ==2
                    original = cat(3, original, original, original);
@@ -202,18 +222,18 @@ for counter = 1:length(imagej_zips) %loop over images
                for i=1:length(ROIs)
                    dum = mask_overlap;
                    dum(mask_overlap~=i)=0;
-                   hold on 
-                   visboundaries(dum,'Color',color_opt{ii},'LineWidth', 1,'LineStyle', '-');
+                   hold on
+                   visboundaries(dum,'Color',color_opt{c_ind},'LineWidth', 1,'LineStyle', '-');
                end
                subplot(1,2,1);imshow(original);title({'cropped image'},'FontSize', 22);
-    
-               save_path_overlay = strcat(results_path,'class_' , string(ii),'_annotations\','overlay','\',raw_imgs(counter).name);
-               % for white border (otherwise the borders will turn balcj after saving)
-               if color_opt{ii}=='#FFFFFF'
+
+               save_path_overlay = strcat(results_path,'class_' , string(c_ind),'_annotations\','overlay','\',raw_imgs(counter).name);
+               % for white border (otherwise the borders will turn balck after saving)
+               if color_opt{c_ind}=='#FFFFFF'
                    set(gcf, 'InvertHardCopy', 'off');
                end
                saveas(fig, save_path_overlay);
-    
+
                mask_overlap = zeros(size_target,size_target);
                mask_overlap_modify = zeros(size_target,size_target);
                mask_overlap_borderremove = zeros(size_target,size_target);
@@ -236,9 +256,10 @@ for counter = 1:length(imagej_zips) %loop over images
    sum_value1 = sum(cell2mat(data(:,2)));
    sum_value2 = sum(cell2mat(data(:,3)));
    sum_value3 = sum(cell2mat(data(:,4)));
+   sum_value4 = sum(cell2mat(data(:,5)));
 
    % Append the sum as the last row (with a label 'Total')
-   data = [data; {'Total', sum_value1, sum_value2, sum_value3, sum_value3}];
+   data = [data; {'Total', sum_value1, sum_value2, sum_value3, sum_value4}];
 
    % Convert the cell array to a table
    T = cell2table(data, 'VariableNames', {'Name', 'total_nuclei', 'class1_nuclei', 'class2_nuclei', 'class3_nuclei'});
